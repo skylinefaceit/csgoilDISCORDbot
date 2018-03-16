6d34135d6e28365bfbaef3e343ebee26791e7f64
Saar Ovadia <36736277+skylinefaceit@users.noreply.github.com>
2018-03-16 17:05:58 +0200

    app.js
    Bug fixes

--- app.js
+++ app.js
@@ -60,10 +60,11 @@ client.on("message", async message => {
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   
-  if(command === "test") {
-    const mm = await message.channel.send("Testing...");
+  if(command === "message") {
+    const userMessage = args.join(" ");
+    const mm = await message.channel.send("Receiving message...");
     setTimeout(function() {
-    mm.edit(`Test completed.`);
+    mm.edit(`You said, ($userMessage) `);
     }, 300);
   }
   
